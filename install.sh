83e139389f7f4adde0e08976c88bb14414879df5
haotianz <haotianz@stanford.edu>
2019-10-20 17:08:12 -0700

    add install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Install Cmake from https://cmake.org/install/
+
+# Install Pybind11 from https://github.com/pybind/pybind11
+
+# Install OpenCV
+
+# Create simlink for numpy include
+ln -s /usr/local/lib/python3.6/site-packages/numpy/core/include/numpy /usr/local/include
+
+
